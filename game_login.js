b21c203713d900436bbfcae6966431604eb889e4
Suryash756 <86527278+Suryash756@users.noreply.github.com>
2023-03-14 07:25:48 -0700

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,9 @@
+function adduser(){
+     player1_name=document.getElementById("input1").value;
+     player2_name=document.getElementById("input2").value;
+
+     localStorage.setItem("player1",player1_name);
+     localStorage.setItem("player2",player2_name);
+
+     window.location="game_page.html";
+}
